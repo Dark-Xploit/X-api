313950b9aa03113357456cc5c5f7fc3240219537
Neeraj <contact@neerajx0.xyz>
2024-04-25 09:38:34 +0530

    Update API rewrite source in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/*",
+        source: "/api/",
         destination: "https://api.thexapi.xyz/", // Replace with your backend URL
       },
     ];
