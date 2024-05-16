113aae039b966940441053863265669a080af9b9
Neeraj <contact@neerajx0.xyz>
2024-05-16 20:38:29 +0530

    refactor: Update table component to improve readability and responsiveness

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -7,60 +7,40 @@ interface TableProps {
 
 function Table({ selectedApi }: TableProps) {
   return (
-    <table className="w-full">
-      <thead className="w-full">
-        <tr className="w-full justify-between flex border-x border-t pt-2">
-          <th className="w-full flex flex-col border-r justify-center">
-            <div className="w-full border-b p-2">API</div>
-            {services[selectedApi].map((item) => (
-              <div key={item.name} className="w-full border-b p-2 flex justify-center">
-                {item.name}
-              </div>
-            ))}
-          </th>
-          <th className="w-full flex flex-col border-r justify-center">
-            <div className="w-full border-b p-2">DESCRIPTION</div>
-            {services[selectedApi].map((item) => (
-              <div key={item.name} className="w-full border-b p-2 flex justify-center">
-                {item.description}
-              </div>
-            ))}
-          </th>
-          <th className="w-full flex flex-col border-r justify-center">
-            <div className="w-full border-b p-2">METHOD</div>
-            {services[selectedApi].map((item) => (
-              <div key={item.name} className="w-full border-b p-2 flex justify-center">
-                {item.method}
-              </div>
-            ))}
-          </th>
-          <th className="w-full flex border-r flex-col justify-center">
-            <div className="w-full border-b p-2">PARAMS</div>
-            {services[selectedApi].map((item) => (
-              <div key={item.name} className="flex border-b">
-                {Object.keys(item.parameters).map((key) => (
-                  <div key={key} className="w-full p-2 justify-center">
-                    {key}
-                  </div>
-                ))}
-              </div>
-            ))}
-          </th>
-          <th className="w-full flex flex-col justify-center">
-            <div className="w-full border-b p-2">ENDPOINT</div>
-            {services[selectedApi].map((item) => (
-              <a
-                key={item.name}
-                href={BASE_URL + item.url}
-                className="w-full font-mono text-red-400 border-b p-2 flex justify-center"
-              >
-                {item.endpoint}
-              </a>
-            ))}
-          </th>
-        </tr>
-      </thead>
-    </table>
+    <div className="overflow-x-auto">
+      <table className="w-full border-collapse border">
+        <thead>
+          <tr className="text-left">
+            <th className="border p-2">API</th>
+            <th className="border p-2">DESCRIPTION</th>
+            <th className="border p-2">METHOD</th>
+            <th className="border p-2">PARAMS</th>
+            <th className="border p-2">ENDPOINT</th>
+          </tr>
+        </thead>
+        <tbody>
+          {services[selectedApi].map((item) => (
+            <tr key={item.name}>
+              <td className="border p-2">{item.name}</td>
+              <td className="border p-2">{item.description}</td>
+              <td className="border p-2">{item.method}</td>
+              <td className="border p-2">
+                <ul className="list-disc list-inside">
+                  {Object.keys(item.parameters).map((key) => (
+                    <li key={key}>{key}</li>
+                  ))}
+                </ul>
+              </td>
+              <td className="border p-2">
+                <a href={BASE_URL + item.url} className="text-red-400">
+                  {item.endpoint}
+                </a>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
