461c9f9a0827c7514b920ecf4fd09410bf10cb06
Neeraj <contact@neerajx0.xyz>
2024-05-18 14:16:42 +0530

    refactor: Update table component styles for better readability and responsiveness

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -7,31 +7,31 @@ interface TableProps {
 
 function Table({ selectedApi }: TableProps) {
   return (
-    <div className="overflow-x-auto">
-      <table className="w-full border-collapse border">
+    <div className="overflow-x-auto text-neutral-200">
+      <table className="w-full border-collapse  border-neutral-200 border">
         <thead>
           <tr className="text-left">
-            <th className="border p-2">API</th>
-            <th className="border p-2">DESCRIPTION</th>
-            <th className="border p-2">METHOD</th>
-            <th className="border p-2">PARAMS</th>
-            <th className="border p-2">ENDPOINT</th>
+            <th className="border p-2  border-neutral-200 ">API</th>
+            <th className="border p-2  border-neutral-200 ">DESCRIPTION</th>
+            <th className="border p-2  border-neutral-200 ">METHOD</th>
+            <th className="border p-2  border-neutral-200 ">PARAMS</th>
+            <th className="border p-2  border-neutral-200 ">ENDPOINT</th>
           </tr>
         </thead>
         <tbody>
           {services[selectedApi].map((item) => (
             <tr key={item.name}>
-              <td className="border p-2">{item.name}</td>
-              <td className="border p-2">{item.description}</td>
-              <td className="border p-2">{item.method}</td>
-              <td className="border p-2">
+              <td className="border p-2  border-neutral-200 ">{item.name}</td>
+              <td className="border p-2  border-neutral-200 ">{item.description}</td>
+              <td className="border p-2  border-neutral-200 ">{item.method}</td>
+              <td className="border p-2  border-neutral-200 ">
                 <ul className="list-disc list-inside">
                   {Object.keys(item.parameters).map((key) => (
                     <li key={key}>{key}</li>
                   ))}
                 </ul>
               </td>
-              <td className="border p-2">
+              <td className="border p-2  border-neutral-200 ">
                 <a
                   target="_blank"
                   rel="noreferrer noopener"
